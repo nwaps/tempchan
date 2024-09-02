10cbd86dbb5c01d860facf9dfa6be72804a38737
sbook <lestersulair@gmail.com>
2024-09-02 14:02:45 +0000

    frontend changes merged in

---
+++ public/js/banners.js
@@ -0,0 +1,8 @@
+/*                              BANNERS.JS
+  Contains the buffer of strings denoting the different banner files used by
+  the front end client. To add/remove banners, add/remove the desired banner
+  image to public/images/banners, then add the file name to the "banners"
+  array in this file.
+*/
+
+var banners = [];
---
+++ public/js/sticker_list.js
@@ -0,0 +1,8 @@
+/*                              STICKER_LIST.JS
+  Contains the buffer of strings denoting the different stickers used by
+  the front end client. To add/remove stickers, add/remove the desired sticker
+  image to public/images/stickers, then add the file name to the "stickers"
+  array in this file.
+*/
+
+var stickers = [];
