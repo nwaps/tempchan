5c54c6ee2fb5b82bf3eb51d02189c16aabe637c2
sbook <lestersulair@gmail.com>
2024-12-02 06:12:05 +0000

    just putting notes for how it mght work in the future

--- src/sockets/emit_chat.ts
+++ src/sockets/emit_chat.ts
@@ -12,38 +12,43 @@ export default (board: string, data: any) => {
     socket_data.io?.sockets.in(board).emit('chat', JSON.stringify(data));
 
     if (data.from_discord) return
+    console.log(data.chat)
 
-    get_all_settings()
-        .then((settings) => {
-            settings.forEach(set => {
-                const webhookurl = set.settings.webhooks.url.toString()
-                // console.log(webhookurl)
-                const webhook = new WebhookClient({ url: webhookurl })
-                // console.log(set.settings.webhook)
+    if (data.chat == "General") { // global chat, send to all
+        get_all_settings()
+            .then((settings) => {
+                settings.forEach(set => {
+                    const webhookurl = set.settings.webhooks.url.toString()
+                    // console.log(webhookurl)
+                    const webhook = new WebhookClient({ url: webhookurl })
+                    // console.log(set.settings.webhook)
 
-                webhook.edit({
-                    name: data.name,
-                    avatar: 'https://litterbox.catbox.moe/resources/qts/1458602218407.png',
-                }).then(() => {
-                    // Create an object for the webhook message options
-                    const options: WebhookMessageCreateOptions = {};
+                    webhook.edit({
+                        name: data.name,
+                        avatar: 'https://litterbox.catbox.moe/resources/qts/1458602218407.png',
+                    }).then(() => {
+                        // Create an object for the webhook message options
+                        const options: WebhookMessageCreateOptions = {};
 
-                    if (data.body !== undefined) {
-                        options.content = data.body;
-                    }
+                        if (data.body !== undefined) {
+                            options.content = data.body;
+                        }
 
-                    if (data.image) {
-                        // options.content += `\n\nThis message contains an image from livechan. [Upgrade to gold to view](http://livechan.goodhew.lol:3000/chat/${data.board})`
-                        options.files = [{
-                            attachment: data.image
-                        }];
-                    }
+                        if (data.image) {
+                            // options.content += `\n\nThis message contains an image from livechan. [Upgrade to gold to view](http://livechan.goodhew.lol:3000/chat/${data.board})`
+                            options.files = [{
+                                attachment: data.image
+                            }];
+                        }
 
-                    // Send the webhook message
-                    webhook.send(options);
-                }).catch(console.error);
+                        // Send the webhook message
+                        webhook.send(options);
+                    }).catch(console.error);
+                })
             })
-        })
+    } else {
+        // local chat send to specific server
+    }
 
 
 
