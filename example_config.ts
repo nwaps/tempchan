5dc297a1287833c07cbfdcedf28036bc049225c6
sbook <lestersulair@gmail.com>
2024-12-02 05:35:10 +0000

    dot env for token instead of putting it in config so i don't leak it accidentally

--- example_config.ts
+++ example_config.ts
@@ -3,6 +3,9 @@
   various configuration variables used throughout the backend server. FOR
   CHANGES IN THIS FILE TO TAKE EFFECT, THE SERVER MUST BE RESTARTED.
 */
+import * as dotenv from 'dotenv';
+
+dotenv.config()
 
 export default {
     PORT: 3000,
@@ -12,24 +15,22 @@ export default {
     POST_COOLDOWN: 3000,
     // File size limit (in bytes)
     FILESIZE_LIMIT: 1048576 * 6,
-    DISCORD_TOKEN: "",
+    DISCORD_TOKEN: process.env.DISCORD_TOKEN,
     CLIENTID: "",
-    GUILDID: "",
-    WEBHOOK: "",
 
 
     // List of boards supported by the server. Add or remove board names
     // to change the number of boards supported.
-    boards: [ 'int', 'g' ],
+    boards: ['int', 'g'],
 
     // Supported file types
     image_formats: ['jpg', 'jpeg', 'png', 'gif', 'webp'],
     video_formats: ['ogv', 'webm', 'mp4'],
     audio_formats: ['ogg', 'mp3', 'flac'],
-    
+
     codec_names: {
-      "video": ["H.264", "H.265", "VP8", "VP9", "AV1"],
-      "audio": ["AAC", "MP3", "Opus", "Vorbis", "FLAC"]
+        "video": ["H.264", "H.265", "VP8", "VP9", "AV1"],
+        "audio": ["AAC", "MP3", "Opus", "Vorbis", "FLAC"]
     },
-  
+
 };
