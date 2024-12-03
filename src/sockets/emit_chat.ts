26fd3bdb23a0677b95157f930f25298158765c92
sbook <lestersulair@gmail.com>
2024-12-03 13:12:15 +0000

    remove general only whitelist

--- src/sockets/emit_chat.ts
+++ src/sockets/emit_chat.ts
@@ -11,36 +11,36 @@ import { get_all_settings } from "../discord/util/settings";
 export default (board: string, data: any) => {
     socket_data.io?.sockets.in(board).emit('chat', JSON.stringify(data));
 
-    if (data.chat == "General") { // global chat, send to all
-        get_all_settings()
-            .then((settings) => {
-                settings.forEach(set => {
-                    if(set.settings.channels.monoko == "" || set.settings.channels.Monoko == null) return
-                    if (set.guildId == data.from_discord) return
-                    const webhookurl = set.settings.webhooks.url.toString()
-                    const webhook = new WebhookClient({ url: webhookurl })
-                    const options: WebhookMessageCreateOptions = {};
-                    options.username = data.name || "Monoko";
-                    options.avatarURL = data.discord_avatar || 'https://cdn.discordapp.com/avatars/1313062635763138562/e3254f51c050bb0d5ddfe5d21418a387.webp'
+    // if (data.chat == "General") { // global chat, send to all
+    get_all_settings()
+        .then((settings) => {
+            settings.forEach(set => {
+                if (set.settings.channels.monoko == "" || set.settings.channels.Monoko == null) return
+                if (set.guildId == data.from_discord) return
+                const webhookurl = set.settings.webhooks.url.toString()
+                const webhook = new WebhookClient({ url: webhookurl })
+                const options: WebhookMessageCreateOptions = {};
+                options.username = data.name || "Monoko";
+                options.avatarURL = data.discord_avatar || 'https://cdn.discordapp.com/avatars/1313062635763138562/e3254f51c050bb0d5ddfe5d21418a387.webp'
 
-                    if (data.body !== undefined) {
-                        options.content = data.body;
-                    }
-                    const url = `https://monoko.chat/chat/${data.board}`
-                    if (data.image) {
-                        options.content += `\n\nThis message contains an image from Monoko. [Upgrade to gold to view](<${url}>)`
-                        // options.files = [{
-                        // attachment: data.image
-                        // }];
-                    }
+                if (data.body !== undefined) {
+                    options.content = data.body;
+                }
+                const url = `https://monoko.chat/chat/${data.board}`
+                if (data.image) {
+                    options.content += `\n\nThis message contains an image from Monoko. [Upgrade to gold to view](<${url}>)`
+                    // options.files = [{
+                    // attachment: data.image
+                    // }];
+                }
 
-                    // Send the webhook message
-                    webhook.send(options);
-                })
+                // Send the webhook message
+                webhook.send(options);
             })
-    } else {
-        // local chat send to specific server
-    }
+        })
+    // } else {
+    // local chat send to specific server
+    // }
 
 
 
